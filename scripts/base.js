151e8030456375d783d654bc0113d25385919b47
Mike Babb <mike7400@gmail.com>
2020-03-18 13:58:00 -0400

    documentation fixes.

--- scripts/base.js
+++ scripts/base.js
@@ -13,20 +13,23 @@ Qualtrics.SurveyEngine.addOnUnload(function() {});
 
 document.getElementById("speedtest-frame").addEventListener("load", function() {
     var w = this.contentWindow;
-    // Initialize the call to the speed test app.
+    // Initialize the call to the speed test app; the below calls the dev version thereof.
     w.postMessage("start", "https://mbabb.fi.ncsu.edu/speedtest/");
 });
 
 function receiveMessage(event) {
+    // If our Qualtrics context is defined, and we've yet to set any embedded data, continue onward.
     if (that !== null && event.data !== null && !set) {
+        // Coalesce our speed test data object into a dictionary.
         let speedtestData = JSON.parse(event.data);
         set = true;
 
+        // Sanity check, to be removed in the future.
         console.log(speedtestData.dlStatus);
 
         /**
          * The below sets the embedded data fields within the survey with the
-         * syntax of (embedded_data_filed_name, value).
+         * syntax of (embedded_data_field_name, value).
          * Note that the Qualtrics survey context of 'that' cannot be used here:
          * one must used the 'Qualtrics.SurveyEngine' object.
          */
@@ -50,6 +53,7 @@ function receiveMessage(event) {
             "ip_address",
             speedtestData["ip"]
         );
+        // For the auto progression; simulates a click of the next button based on the 'that' context.
         that.clickNextButton();
     }
 }
