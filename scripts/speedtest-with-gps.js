3a759f66ae7774e26a9d10c7d831467047aeafab
Mike Babb <mike7400@gmail.com>
2020-07-16 22:48:41 -0400

    removed comments

--- scripts/speedtest-with-gps.js
+++ scripts/speedtest-with-gps.js
@@ -1,6 +1,5 @@
 //@ts-ignore
 const qs = Qualtrics.SurveyEngine;
-// The speedtest iframe name. This must match the Qualtrics HTML!
 const iframeId = "speedtest-frame";
 // const speedtestURL = "https://speedtest.fi.ncsu.edu/general_bb/";
 const speedtestURL = "https://mbabb.fi.ncsu.edu/speedtest/sites/index.html";
@@ -11,16 +10,11 @@ const receiveMessage = function (event) {
     if (windowMessage != null && windowMessage.key === WINDOW_KEY) {
         if (windowMessage.message === "complete") {
             const { dlStatus, ulStatus, pingStatus, jitterStatus, ip } = windowMessage.data;
-            /**
-             * The below sets the embedded data fields within the survey with the
-             * syntax of (embedded_data_field_name, value).
-             */
             qs.setEmbeddedData("dl_speed", dlStatus);
             qs.setEmbeddedData("ul_speed", ulStatus);
             qs.setEmbeddedData("ping", pingStatus);
             qs.setEmbeddedData("jitter", jitterStatus);
             qs.setEmbeddedData("ip_address", ip);
-            // For the auto progression; simulates a click of the next button based on the 'that' context.
             this.showNextButton();
         }
         else if (windowMessage.message === "next") {
