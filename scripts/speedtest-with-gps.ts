ec2a4a9fee65140e1027d1aad742229135fd61b7
Mike Babb <mike7400@gmail.com>
2020-07-16 22:37:37 -0400

    added typescript support

--- scripts/speedtest-with-gps.ts
+++ scripts/speedtest-with-gps.ts
@@ -4,13 +4,29 @@ const qs = Qualtrics.SurveyEngine;
 const iframeId = "speedtest-frame";
 // const speedtestURL = "https://speedtest.fi.ncsu.edu/general_bb/";
 const speedtestURL = "https://mbabb.fi.ncsu.edu/speedtest/sites/index.html";
+
+interface IWindowMessage {
+    message: string;
+    key: string;
+    data: { [arg: string]: string };
+}
+
 const WINDOW_KEY = "password";
-const receiveMessage = function (event) {
-    const windowMessage = event.data;
+
+const receiveMessage = function (event: MessageEvent) {
+    const windowMessage: IWindowMessage = event.data;
     console.log(windowMessage);
+
     if (windowMessage != null && windowMessage.key === WINDOW_KEY) {
         if (windowMessage.message === "complete") {
-            const { dlStatus, ulStatus, pingStatus, jitterStatus, ip } = windowMessage.data;
+            const {
+                dlStatus,
+                ulStatus,
+                pingStatus,
+                jitterStatus,
+                ip
+            } = windowMessage.data;
+
             /**
              * The below sets the embedded data fields within the survey with the
              * syntax of (embedded_data_field_name, value).
@@ -22,29 +38,35 @@ const receiveMessage = function (event) {
             qs.setEmbeddedData("ip_address", ip);
             // For the auto progression; simulates a click of the next button based on the 'that' context.
             this.showNextButton();
-        }
-        else if (windowMessage.message === "next") {
+        } else if (windowMessage.message === "next") {
             this.clickNextButton();
         }
     }
 };
+
 qs.addOnload(function () {
     window.scrollTo(0, 0);
     window.addEventListener("message", receiveMessage.bind(this));
+
     this.hideNextButton();
-    const windowMessage = {
+
+    const windowMessage: IWindowMessage = {
         message: "start",
         key: "password",
         data: {}
     };
+
     const start = function () {
         document.getElementById(iframeId).addEventListener("load", function (event) {
-            const iframe = event.target;
+            const iframe = <HTMLIFrameElement>event.target;
             console.log(iframe.id);
             iframe.contentWindow.postMessage(windowMessage, speedtestURL);
         });
     };
+
     setTimeout(start, 10);
 });
-qs.addOnReady(function () { });
-qs.addOnUnload(function () { });
+
+qs.addOnReady(function () {});
+
+qs.addOnUnload(function () {});
